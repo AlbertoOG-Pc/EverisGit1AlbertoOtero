6bdb8f3b9d596d5a682a49bab29dfcf44136f130
Alberto Otero <alberto211994@gmail.com>
2021-10-25 13:52:43 +0200

    Segundo commit: Cambio de saludo

--- src/main/java/com/everis/App.java
+++ src/main/java/com/everis/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hola soy Alberto Otero Gonzalez, Desarrollador de este proyecto" );
     }
 }
